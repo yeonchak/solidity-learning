8483012903611926ce1fe564705509b4f5ae72a8
yeonchak <kangjiyeon030408@gmail.com>
2026-07-01 12:55:26 +0900

    added approve and transferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,13 +5,16 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
     uint8 public decimals; // uint8 --> 8 bit unsigned int, uint16, ... , uint256
 
     uint256 public totalSupply; // 전체 몇개를 발행했느냐
     mapping(address => uint256) public balanceOf; // 누가 얼마를 가지고 있느냐
-    // 수수료를 내지 아니함
+    mapping(address => mapping(address => uint256)) allowance;
 
     constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount){
         name = _name;
@@ -22,9 +25,25 @@ contract MyToken {
         _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1MT
     }
 
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+
+        emit Transfer(address(0), owner, amount);
     }
 
     function transfer(uint256 amount, address to) external{
@@ -32,16 +51,30 @@ contract MyToken {
 
         balanceOf[msg.sender] -= amount; // state 변경으로 gas 발생
         balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
     }
-    // function totalSupply() external view returns (uint256) { // external : 외부에서만 호출 가능, view : 읽기전용
-    //     return totalSupply;
-    // }
- 
-    // function balanceOf(address owner) external view returns (uint256){
-    //     return balanceOf[owner];
-    // }
- 
-    // function name() external view returns(stirng memory) {
-    //     return name;
-    // }
+    /* 
+    function totalSupply() external view returns (uint256) { // external : 외부에서만 호출 가능, view : 읽기전용
+        return totalSupply;
+    }
+    function balanceOf(address owner) external view returns (uint256){
+        return balanceOf[owner];
+    }
+    function name() external view returns(stirng memory) {
+        return name;
+    } 
+    */
 }
+
+/*
+approve
+ - allow spender address to send my token
+transferForm
+ - spender: owner -> target address
+
+ * token owner --> bank contract
+ * token owner --> router contract --> bank contract
+ * token owner --> router contract --> bank contract(multi contract)
+
+*/
